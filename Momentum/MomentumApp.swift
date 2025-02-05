304cacc95955b351eaf4643ea46b76ef4e80dcec
Ben Li <benli100304@gmail.com>
2025-02-04 21:20:50 -0500

    Initial Commit

---
+++ Momentum/MomentumApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentumApp.swift
+//  Momentum
+//
+//  Created by Benjamin Li on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct MomentumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
