1dcce99b44694b35c1225b18d17037b8f5be1d51
Ben Li <benli100304@gmail.com>
2025-04-13 02:36:32 -0400

    Untrack secret.swift and update .gitignore

--- frontend/Momentum/secret.swift
+++
@@ -1,4 +0,0 @@
-struct Secrets {
-    static let jwtToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzQ0NjA0Njg4LCJpYXQiOjE3NDQzNDIyMDQsImp0aSI6IjczODQyNGZlZmMxNDRhM2RiYjQwYWRlMjRmZjIyYmZhIiwidXNlcl9pZCI6MX0.YwWR3SLVOY1EMFQpP08GZYyD_RbOP6S1EmCXplp6C5o"
-}
-
